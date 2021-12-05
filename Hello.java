275af73296c727959c9b4703dd240fa49af9be75
LXJEL <lxj2605663319@outlook.com>
2021-12-05 19:22:07 +0800

    testdemo

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package lxj.demo;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello java!");
+    }
+}
